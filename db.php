684a0d3092424969b5cefa1ad1b56422037c9ba3
Alice <virginial2123@gmail.com>
2020-04-22 15:36:49 +0800

    各資料表新增語法 members

---
+++ db.php
@@ -0,0 +1,4 @@
+<?php
+
+$members_sql = 'INSERT INTO `members`(`sid`, `email`, `password`, `mobile`, `name`, `created_date`, `receiver`, `receiver_mobile`, `address`, `branch`) 
+VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])';
