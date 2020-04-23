f97c4248c467e0165c04554020544527b46da343
Alice <virginial2123@gmail.com>
2020-04-23 17:57:26 +0800

    sql

---
+++ db/color.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+`color`(`pro_sid`, `color`, `en_color`,  `pro_pic`)
+VALUES
+('1', 'bk', 'black', '(paddedbra_000_bk_1, paddedbra_000_bk_2, paddedbra_000_bk_3, paddedbra_000_bk_4, paddedbra_000_bk_5)'),
+('1', 'pk', 'pink', '(paddedbra_000_pk_1, paddedbra_000_pk_2, paddedbra_000_pk_3, paddedbra_000_pk_4, paddedbra_000_pk_5)'),
+('2', 'gr', 'gray', '(paddedbra_001_gr_1, paddedbra_001_gr_2, paddedbra_001_gr_3, paddedbra_001_gr_4, paddedbra_001_gr_5)'),
+('2', 'pk', 'pink', '(paddedbra_001_pk_1, paddedbra_001_pk_2, paddedbra_001_pk_3, paddedbra_001_pk_4, paddedbra_001_pk_5)'),
+('3', 'bk', 'black', '(paddedbra_002_bk_1,paddedbra_002_bk_2, paddedbra_002_bk_3, paddedbra_002_bk_4, paddedbra_002_bk_5)'),
+('3', 'w', 'white', '(paddedbra_002_w_1,paddedbra_002_w_2, paddedbra_002_w_3, paddedbra_002_w_4, paddedbra_002_w_5)'),
+('3', 'br', 'brown', '(paddedbra_002_br_1,paddedbra_002_br_2, paddedbra_002_br_3, paddedbra_002_br_4, paddedbra_002_br_5)'),
+('4', 'bk', 'black', '(paddedbra_003_bk_1, paddedbra_003_bk_2, paddedbra_003_bk_3, paddedbra_003_bk_4, paddedbra_003_bk_5)'),
+('5', 'pk', 'pink', '(paddedbra_004_pk_1, paddedbra_004_pk_2, paddedbra_004_pk_3, paddedbra_004_pk_4, paddedbra_004_pk_5)'),
+('5', 'y', 'yellow', '(paddedbra_004_y_1, paddedbra_004_y_2, paddedbra_004_y_3, paddedbra_004_y_4, paddedbra_004_y_5)'),
+('6', 'w', 'white', '(paddedbra_005_w_1, paddedbra_005_w_2, paddedbra_005_w_3, paddedbra_005_w_4, paddedbra_005_w_5)'),
+('7', 'bl', 'blue', '(paddedbra_006_bl_1, paddedbra_006_bl_2, paddedbra_006_bl_3, paddedbra_006_bl_4, paddedbra_006_bl_5)'),
+('8', 'w', 'white', '(paddedbra_007_w_1, paddedbra_007_w_2, paddedbra_007_w_3, paddedbra_007_w_4, paddedbra_007_w_5)'),
+('9', 'bk', 'black', '(paddedbra_008_bk_1, paddedbra_008_bk_2, paddedbra_008_bk_3, paddedbra_008_bk_4, paddedbra_008_bk_5)'),
+('10', 'bk', 'black', '(paddedbra_009_bk_1, paddedbra_009_bk_2, paddedbra_009_bk_3, paddedbra_009_bk_4, paddedbra_009_bk_5)')
+
+-- 以上為 Padded bra 1-10筆-------------------------------------------------
