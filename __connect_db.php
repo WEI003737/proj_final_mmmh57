ac6aa0478b2be61e5590e6d312f0c377229dd06e
WEI <virginial2123@gmail.com>
2020-04-25 17:49:51 +0800

    資料庫連線

---
+++ __connect_db.php
@@ -0,0 +1,24 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'proj_final';
+$db_user = 'root';
+$db_pass = '';
+
+$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $db_host, $db_name);
+
+$pdo_options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+];
+
+try{
+    $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+}catch (PDOException $ex){
+    echo 'Ex:'. $ex->getMessage();
+}
+
+if(! isset($_SESSION)){
+    session_start();
+}
