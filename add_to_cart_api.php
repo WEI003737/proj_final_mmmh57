bda1476070605155a63370443bdd86bea5a19f3d
WEI <virginial2123@gmail.com>
2020-04-27 22:49:37 +0800

    新增商品及庫存數確認

--- add_to_cart_api.php
+++ add_to_cart_api.php
@@ -3,18 +3,30 @@ require __DIR__. '/__connect_db.php';
 
 if(!isset($_SESSION['cart'])){
     $_SESSION['cart'] = [];
+    $_SESSION['cart']['success'] = false;
+
 };
 
 //sid 是 size 的 sid 才能判斷庫存
 $cart_sid = isset($_GET['cart_sid']) ? intval($_GET['cart_sid']) : 0;
 $cart_qty = isset($_GET['cart_qty']) ? intval($_GET['cart_qty']) : 0;
-$cart_color = isset($_GET['cart_color']) ? intval($_GET['cart_color']) : '';
 
+
+//先判斷有無此商品
 if(! empty($cart_sid)){
+    $sizeSidSql = $pdo -> query("SELECT * FROM `size` WHERE `sid`= ".$cart_sid);
+    $haveSizeSid = $sizeSidSql -> fetch(); //設fetchAll不會動
 
-//判斷有沒有該樣商品 & 商品有沒有貨???????????????????????????????????????????????
-    if(!empty($cart_qty))
-    $_SESSION['cart'][$cart_sid] = $cart_qty;
+    if($sizeSidSql -> rowCount() == 1){
+//判斷有無足夠庫存
+        if(!empty($cart_qty) and $cart_qty <= $haveSizeSid['in_stock']) {
+            $_SESSION['cart'][$cart_sid] = $cart_qty;
+            $_SESSION['cart']['success'] = true;
+        }else{
+            $_SESSION['cart']['success'] = false;
+        }
+    }
+
 }else {
     unset($_SESSION['cart'][$cart_sid]);
 };
