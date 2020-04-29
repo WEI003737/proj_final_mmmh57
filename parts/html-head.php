8e023c772391e86cbf6125a82c40c0ea16e77b62
WEI <virginial2123@gmail.com>
2020-04-29 11:57:51 +0800

    購物車 parts

---
+++ parts/html-head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- normalize.css -->
+    <link rel="stylesheet" href="./css/normalize.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+    <!-- --fontawesome-- -->
+    <link rel="stylesheet" href="./fontawesome-free-5.12.1-web/css/all.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>My Cart</title>
+
+</head>
+<body>
+    
