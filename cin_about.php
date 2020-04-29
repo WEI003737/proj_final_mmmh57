115b308db709d46adab0d855df13313dfd009548
WEI <virginial2123@gmail.com>
2020-04-29 23:43:47 +0800

    0429 關於我

---
+++ cin_about.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cin_CSS/cin_about.css">
+    <title>Document</title>
+    <?php include __DIR__. '/parts/h_f_link.php';?>
+
+    <style>
+        /* * {
+            outline: #FA8000 solid 1px;
+        } */
+    </style>
+</head>
+<?php include __DIR__. '/css/cin_about_css.php';?>
+<?php include __DIR__. '/parts/h_f_css.php';?>
+<body>
+<?php include __DIR__. '/parts/header.php';?>
+    <!----------------------   主視覺  -------------------------->
+    <section class="show_desktop cin_aboutBanner">
+        <section class="container">
+            <div class="cin_boxR"></div>
+            <div class="cin_boxL"></div>
+            <p class="cin_about_rightsideText">ABOUT REDCORE</p>
+            <div class="cin_aboutTextWrapper d-flex show_desktop">
+                <div class="cin_aboutLogo">
+                    <img src="./img/about_logo.png" alt="">
+                </div>
+                <p>「動」是一種生活的態度；是一種精神享受!「動」可以使人更自信、樂觀、生活更精彩！<br><br>
+                    希望能讓更多人在這個繁華的時代，享受到「動」的快樂！<br>
+                    本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。<br>
+                    並以舒適、日常好穿搭為主要訴求。讓運動能夠融入日常生活當中，讓運動成為習慣成為「日常」！<br>
+                    RedCore，讓每個女孩由內而外，爆發出自信迷人的光彩!
+                </p>
+            </div>
+        </section>
+    </section>
+
+    <section class="show_mobile cin_aboutBanner_mobile">
+        <section class="container">
+            <div class="cin_boxR"></div>
+            <div class="cin_boxL"></div>
+            <p class="cin_about_rightsideText">ABOUT REDCORE</p>
+            <div class="cin_aboutTextWrapper show_mobile">
+                <div class="cin_aboutLogo">
+                    <img src="./img/about_logo.png" alt="">
+                </div>
+                <p>「動」是一種生活的態度；是一種精神享受!「動」可以使人更自信、樂觀、生活更精彩！<br><br>
+                    希望能讓更多人在這個繁華的時代，享受到「動」的快樂！<br>
+                    本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。<br>
+                    並以舒適、日常好穿搭為主要訴求。讓運動能夠融入日常生活當中，讓運動成為習慣成為「日常」！<br>
+                    RedCore，讓每個女孩由內而外，爆發出自信迷人的光彩!
+                </p>
+            </div>
+        </section>
+    </section>
+
+
+    <!----------------------   理念  -------------------------->
+    <section class="cin_sustain" id="cin_sustain">
+        <div class="cin_sustain_wrapperA" id="cin_sustain_wrapperA">
+            <div class="cin_sustain_wrapperB d-flex justify_between">
+                <img src="./img/about_sustain1.png" alt="">
+                <div class="cin_sustain_mind">
+                    <h2># 理念</h2>
+                    <p>自信的女神會感覺自己充滿能量。賦予自己力量的女神將改變世界。我們的靈感來自於那些自我要求更多的女性、衝破高層無形限制的女性、為自己開闢道路及負責的女性，堅強、有力量、肯付出、充滿火焰、無比努力、堅持不懈，想不斷成為更好的自己的女性。</p>
+                </div>
+            </div>
+            <div class="cin_sustainTitle">
+            <h1>Sustainable Development</h1>
+            </div>
+           <img class="cin_sustain_rec"src="./img/about_sustain_rec.png" alt="">
+        </div>
+
+        <div class="cin_sustain_wrapperC d-flex justify_between">
+            <div class="cin_sustain_mind">
+                <p>為了要讓時尚產品減少對地球的傷害，我們選擇的製造及供應夥伴，都需通過全球紡織業嚴格的環保認證。也就是說，我們的製程與產品都需通過生態環保、健康無害及安全的檢驗。我們使用經過高規格認可的染料，對環境高度友善，減少空氣和水系統中的不良因子，使工廠用更少的資源及能源來清潔廢水。用更安全的化學品，及認證過的工廠環境（通過物理接觸和空氣質量測量），負起對社區周圍水域環境的保護責任。</p>
+            </div>
+            <img class="show_desktop" src="./img/about_sustain2.png" alt=""> 
+        </div>            
+
+    </section>
+    <!----------------------   團隊  -------------------------->
+    <section class="cin_team">
+        <div class="container">
+            <div class="cin_teamTitles text-center">
+                <div class="cin_hLine"></div>
+                <h1>OUR LOVELY TEAM</h1>
+                <h2>我們的團隊</h2>
+            </div>
+            <div class="cin_team_box">
+                <div class="cin_teamPara">
+                    <p>「動」是一種生活的態度；是一種精神享受!「動」可以使人更自信、樂觀、生活更精彩！
+                        希望能讓更多人在這個繁華的時代，享受到「動」的快樂！
+                        本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。本品牌的服飾、周邊融入各式時尚元素，期望能吸引到更多人發現「運動」是件很Fashion的事情。
+                        想加入這個歡樂的家庭嗎？</p>
+                </div>
+                <div class="d-flex justify_between">
+                    <h3>#加入我們吧</br>#Come and join us</h3>
+                    <ul class="icon_show_desktop">
+                        <li><a href=""><img src="./img/about_icon1.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/about_icon2.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/about_icon3.png" alt=""></a></li>
+                    </ul>
+                </div>
+                <div class="cin_team_icons icon_show_mobile">
+                    <a href=""><img src="./img/about_icon1.png" alt=""></a>
+                    <a href=""><img src="./img/about_icon2.png" alt=""></a>
+                    <a href=""><img src="./img/about_icon3.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    <?php include __DIR__. '/parts/footer.php';?>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <?php include __DIR__. '/parts/h_f_script.php';?>
+ <!-- 左側滑入動畫 -->
+<script>
+let targetPosition=$("cin_sustain").offset().top;  
+$(window).on("scroll", function(){
+    let scrollTop=$(this).scrollTop();
+    console.log(scrollTop)
+    if(scrollTop> (targetPosition - 300) ){
+                $(".cin_sustain_rec").addClass("active")
+            }else{
+                $(".cin_sustain_rec").removeClass("active")
+            }
+
+          })
+</script>
+</body>
+
+</html>
---
+++ css/cin_about_css.php
@@ -0,0 +1,466 @@
+<style>
+/*============================= 共用 =================================*/
+            *{
+                box-sizing: border-box;
+            }
+            body{
+                margin: 0;
+            }
+            .container{
+                max-width: 1440px;
+                margin: 0 auto;
+            }  
+            .text-center{
+                text-align: center;
+            }
+            .d-flex{
+                display: flex;
+            }
+            .text-center{
+                text-align: center;
+            }
+            .justify_between{
+                justify-content: space-between;
+            }
+            h1{
+                color: #ca054d;
+                font-size: 60px;
+                font-family:Lucida Sans Demibold Roman;
+                font-style: Demibold Roman;
+                font-weight: 600;
+                margin:0px;
+            }
+            @media screen and (max-width: 360px){
+            h1{
+               font-size: 32px;
+            }
+            }
+            @font-face{
+                font-family: 'Lucida Sans Demibold Roman';
+                src:url(../font/Lucida\ Sans);
+                /* font-style:Demibold; */
+            }
+            h2{
+                color: #272838;
+                font-size: 40px;  
+                margin: 0;
+            }
+            h3{
+                color: #ca054d;
+                font-size: 25px;  
+            }
+            @media screen and (max-width: 360px){
+                h3{
+                font-size: 18px;  
+                }
+            }
+            p{
+                color: #840032;
+            }
+            .show_mobile{
+                display: none !important;
+                    }
+            @media screen and (max-width: 360px){
+                .show_desktop{
+                     display:none!important;
+                }
+                .show_mobile{
+                    display:block!important;
+                }
+            }
+
+           .icon_show_mobile{
+            display: none !important;
+           }
+           @media screen and (max-width: 936px){
+                .icon_show_desktop{
+                     display:none!important;
+                }
+                .icon_show_mobile{
+                    display:block!important;
+                }
+            }
+
+/*============================= 主視覺 =================================*/
+        .cin_aboutBanner{
+            height: 100vh;
+            background: url(./img/aboutBanner.png);
+            background-size: cover;
+            position:relative;
+        }
+        .cin_aboutBanner_mobile{
+            height: 100vh;
+            background: url(./img/aboutBanner_mobile.png);
+            background-size: cover;
+            position:relative;
+        }
+        .cin_boxR{
+            border-right:2px solid #ca054d;
+            border-top:2px solid #ca054d;
+            width: 475px;
+            height: 140px;
+            position: absolute;
+            right: 0;
+            margin: 50px 50px 0 0 ;
+        }
+        @media screen and (max-width: 360px){
+            .cin_boxR{
+                width: 170px;
+                height: 30px; 
+                margin: 25px 25px 0 0 ;
+            }
+        }
+        .cin_boxL{
+            border-left:2px solid #ca054d;
+            border-bottom:2px solid #ca054d;
+            width: 475px;
+            height: 140px;
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            margin: 0 0 50px 50px ;
+        }
+        @media screen and (max-width: 360px){
+            .cin_boxL{
+                width: 170px;
+                height: 30px;
+                margin: 0 0 25px 25px ;
+            }
+        }
+        .cin_about_rightsideText{
+            font-size: 2.5rem;
+            /* -webkit-text-stroke:1.3px #ca054d; */
+            text-shadow: 0 0 2px red, 0 0 5px purple;
+            color:#eee;
+            position: absolute;
+            right: 0;
+            top:52%;
+            right: -130px;
+            transform: rotate(90deg);
+            margin: 10px;
+        }
+        @media screen and (max-width: 360px){
+            .cin_about_rightsideText{
+                -webkit-text-stroke:1px #ca054d;
+                font-size: 2rem;
+                top:30%;
+                right: -116px;
+            }
+        }
+        .cin_aboutTextWrapper p{
+            padding: 10px;
+        }
+        .cin_aboutLogo img{
+            width: 160px;
+            padding: 10px;
+            margin-right: 15px;
+        }
+        @media screen and (max-width: 1010px){
+            .cin_aboutLogo img{
+                width: 130px;
+                padding: 5px;
+            }
+        }
+        @media screen and (max-width: 725px){
+            .cin_aboutLogo img{
+               display: none;
+            }
+        }
+        @media screen and (max-width: 360px){
+            .cin_aboutLogo img{
+               display: block;
+               width: 80px;
+               margin: 10px auto;
+            }
+        }
+        .cin_aboutTextWrapper{
+            position: absolute;
+            width: 55%;
+            top: 40%;
+            left: 25%;
+        }
+        @media screen and (max-width: 1010px){
+            .cin_aboutTextWrapper{
+                position: absolute;
+                width: 70%;
+                top: 35%;
+                left: 15%;
+            }
+        }
+        @media screen and (max-width: 825px){
+            .cin_aboutTextWrapper{
+                position: absolute;
+                width: 75%;
+                top: 35%;
+                left: 15%;
+            }
+        } @media screen and (max-width: 360px){
+            .cin_aboutTextWrapper{
+                /* position: absolute; */
+                width: 64%;
+                top: 10%;
+                left: 20%;
+            }
+        }
+        .cin_aboutTextWrapper p{
+            margin: 0;
+        }
+/*============================= 理念 =================================*/
+.cin_sustain{
+    height: 125vh;
+    position: relative;
+    max-width: 1920px;
+}
+@media screen and (max-width: 1300px){
+    .cin_sustain{
+        height: 110vh;
+}
+}
+@media screen and (max-width: 970px){
+    .cin_sustain{
+        height: 100vh;
+}
+}
+@media screen and (max-width: 800px){
+    .cin_sustain{
+        height: 86vh;
+}
+}@media screen and (max-width: 360px){
+    .cin_sustain{
+        height: 130vh;
+}
+}
+.cin_sustain_wrapperA{
+    height: 900px;
+    position: relative;
+    margin-top:200px;
+}
+@media screen and (max-width: 970px){
+    .cin_sustain_wrapperA{
+    margin-top:100px;
+}
+}
+@media screen and (max-width: 360px){
+    .cin_sustain_wrapperA{
+    margin-top:0;
+}
+}
+.cin_sustain_rec{
+   width: 100%;
+   top:57%;
+   position: absolute;
+}
+@media screen and (max-width: 970px){
+.cin_sustain_rec{
+    height: 250px;
+}
+}
+@media screen and (max-width: 800px){
+.cin_sustain_rec{
+    height: 300px;
+    top:47%;
+}
+}
+@media screen and (max-width: 360px){
+.cin_sustain_rec{
+    top:37%;
+    height: 50%;
+}
+}
+.cin_sustainTitle{
+    top:54%;
+    position: absolute;
+    margin-left: 20%;
+}
+@media screen and (max-width: 996px){
+    .cin_sustainTitle{
+        margin-left: 0;
+    }
+}
+@media screen and (max-width: 800px){
+    .cin_sustainTitle{
+        top:43%;
+    }
+}
+.cin_sustainTitle h1{
+    line-height: 40px;
+}
+@media screen and (max-width: 800px){
+    .cin_sustainTitle h1{
+        font-size:50px;
+    }
+}
+@media screen and (max-width: 360px){
+    .cin_sustainTitle{
+        top:35%;
+    }
+}
+.cin_sustain_wrapperB{
+    max-width: 1100px;
+    margin: 100px auto;
+}
+@media screen and (max-width: 1300px){
+    .cin_sustain_wrapperB{
+    max-width: 1000px;
+    }
+}
+@media screen and (max-width: 970px){
+    .cin_sustain_wrapperB{
+    max-width: 900px;
+    }
+}
+@media screen and (max-width: 360px){
+    .cin_sustain_wrapperB{
+        margin: 40px auto;
+    }
+}
+.cin_sustain .cin_sustain_wrapperB img{
+    max-width: 500px;
+    max-height: 650px;
+}
+@media screen and (max-width: 1300px){
+    .cin_sustain .cin_sustain_wrapperB img{
+    max-width: 400px;
+    max-height: 550px;
+}
+}
+@media screen and (max-width: 970px){
+    .cin_sustain .cin_sustain_wrapperB img{
+    max-width: 300px;
+    max-height: 450px;
+}
+}
+@media screen and (max-width: 800px){
+    .cin_sustain .cin_sustain_wrapperB img{
+    max-width: 200px;
+    max-height: 350px;
+    object-fit:cover;
+}
+}
+@media screen and (max-width: 360px){
+    .cin_sustain .cin_sustain_wrapperB img{
+        max-width: 100px;
+        max-height: 350px;
+        object-fit:cover;
+    }
+}
+.cin_sustain_mind{
+    max-width:500px;
+    margin: 15% 0;
+    z-index: 1;
+}
+@media screen and (max-width: 800px){
+    .cin_sustain_mind{
+        margin: 10% 0;
+    }
+}
+@media screen and (max-width: 360px){
+    .cin_sustain_mind{
+        max-width:200px;
+        margin: 0 10px;
+    }
+}
+.cin_sustain_wrapperC{
+    max-width: 1100px;
+    margin: -570px auto;
+}
+
+.cin_sustain .cin_sustain_wrapperC img{
+    max-height: 700px;
+    z-index: 100;
+}
+@media screen and (max-width: 1300px){
+    .cin_sustain .cin_sustain_wrapperC img{
+        max-height: 600px;
+    }
+}
+@media screen and (max-width: 970px){
+    .cin_sustain .cin_sustain_wrapperC img{
+        max-height: 500px;
+    }
+}
+@media screen and (max-width: 800px){
+    .cin_sustain .cin_sustain_wrapperC img{
+        display:none;
+    }
+}
+.cin_sustain_wrapperC .cin_sustain_mind p{
+color:#eee;
+margin-top: 100px;
+}
+@media screen and (max-width: 360px){
+    .cin_sustain_wrapperC .cin_sustain_mind p{
+        width: 300px;
+        margin: 120px 30px;
+    }
+}
+/*============================= 團隊 =================================*/
+.cin_team{
+    height: 140vh;
+}
+.cin_teamTitles{
+    margin: 70px auto;
+}
+@media screen and (max-width: 360px){
+    .cin_teamTitles{
+        margin: 30px auto;
+    }
+}
+.cin_hLine{
+    width: 100px;
+    height: 4px;
+    background: #ca054d;
+    margin: 30px auto;
+}
+.cin_team_box{
+    max-width: 1440px;
+    height:480px;
+    background: #FDF4ED;
+    padding: 80px;
+    margin-bottom:200px;
+}
+@media screen and (max-width: 740px){
+    .cin_team_box{
+        height:580px;
+    }
+}
+@media screen and (max-width: 580px){
+    .cin_team_box{
+        height:680px;
+    }
+}
+@media screen and (max-width: 360px){
+    .cin_team_box{
+        padding: 30px;
+        height:110vh;
+        margin-bottom:60px;
+    }
+}
+.cin_teamPara{
+    max-width: 1100px;
+    margin-bottom:100px;
+}
+@media screen and (max-width: 936px){
+    .cin_teamPara{
+        margin-bottom:0px;
+}
+}
+@media screen and (max-width: 360px){
+    .cin_teamPara{
+    margin-bottom:60px;
+}
+}
+@media screen and (max-width: 360px){
+    .cin_team_box .cin_teamPara p{
+        width: 280px;
+        margin: 0 auto;
+    }
+}
+.cin_team_box li{
+    list-style: none;
+    margin-bottom: 10px;
+}
+.cin_team_icons a{
+    margin:5px;
+}
+</style>
