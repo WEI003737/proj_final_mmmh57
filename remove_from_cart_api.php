2da6deba2c99891e18374760a4c62924d681cbc5
WEI <virginial2123@gmail.com>
2020-05-04 23:07:50 +0800

    0504 移除商品api

---
+++ remove_from_cart_api.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__. '/__connect_db.php';
+
+
+
+$cart_sid = isset($_GET['cart_sid']) ? intval($_GET['cart_sid']) : 0;
+
+
+unset($_SESSION['cart'][$cart_sid]);
+
+header('Content-Type: application/json');
+echo json_encode($_SESSION['cart']); //送關聯式陣列回來
