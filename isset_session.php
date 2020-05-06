760292c46fd236f96fad8b014ab46eb48a2ae825
Alice <virginial2123@gmail.com>
2020-05-06 11:45:16 +0800

    0506 確認有無session

---
+++ isset_session.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+$output=[
+    "loginUser" => false,
+    "cart" => false,
+    "customized" => false,
+];
+
+$loginUser = isset($_SESSION["loginUser"])? $_SESSION["loginUser"] : "";
+$cart = isset($_SESSION["cart"]) ? $_SESSION["cart"] : "";
+$customized = isset($_SESSION["customized"]) ? $_SESSION["customized"] : "";
+
+if(!empty($loginUser)){
+    $output["loginUser"] = true;
+};
+if(!empty($cart)){
+    $output["cart"] = true;
+};
+if(!empty($customized)){
+    $output["customized"] = true;
+};
+
+header('Content-Type: application/json');
+echo json_encode($output);
+
+?>
+
+
+
+
+
